dfd45191803c2bd09a1eea22a6697fdcab8dc80f
Dina <154764296+DinaHatrak@users.noreply.github.com>
2026-02-27 06:48:58 +0300

    Update nist_check.py

--- src/nist_check.py
+++ src/nist_check.py
@@ -1,7 +1,18 @@
-def check_password(password: str) -> bool:
- 
+def check_password_nist(password: str):
+
+    reasons = []
+    
+    # Length Check
     if len(password) < 8:
-        return False
-    if password.isalpha() or password.isnumeric():
-        return False
-    return True
+        reasons.append("Password is too short. It should be at least 8 characters.")
+        
+    # Complexity Check
+    if password.isalpha():
+        reasons.append("Add numbers or symbols to make it stronger.")
+    if password.isnumeric():
+        reasons.append("Add letters to prevent easy cracking.")
+
+    if not reasons:
+        return True, "Your password follows NIST standards. Well done!"
+    else:
+        return False, reasons
