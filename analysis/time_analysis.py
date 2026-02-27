c7b59534908ef019a2bd752fb90310b10c629cb0
Dina <154764296+DinaHatrak@users.noreply.github.com>
2026-02-27 06:52:19 +0300

    Add function to analyze upgrade impact on entropy

--- analysis/time_analysis.py
+++ analysis/time_analysis.py
@@ -1,1 +1,3 @@
-
+def analyze_upgrade_impact(current_entropy):
+      upgraded_entropy = current_entropy + 15 
+    return f"By adding 2 more characters, your entropy would jump to {upgraded_entropy} bits!"
