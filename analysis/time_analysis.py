8f2e230002f2b19aff98eacf68f16fc8296003e7
Dina <154764296+DinaHatrak@users.noreply.github.com>
2026-03-23 01:15:47 +0300

    Delete analysis/time_analysis.py

--- analysis/time_analysis.py
+++
@@ -1,3 +0,0 @@
-def analyze_upgrade_impact(current_entropy):
-      upgraded_entropy = current_entropy + 15 
-    return f"By adding 2 more characters, your entropy would jump to {upgraded_entropy} bits!"
