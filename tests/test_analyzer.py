ca52e762fd75d91a4b1cc597e811935e49d1c2f1
Dina <154764296+DinaHatrak@users.noreply.github.com>
2026-03-23 01:19:01 +0300

    Update test cases for password validation

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -6,12 +6,12 @@ class TestPasswordProject(unittest.TestCase):
 
     def test_nist_weak_password(self):
         # Testing a password that is too short
-        is_valid, msg = check_password_nist("123")
+        is_valid, msg = check_password_nist("12345")
         self.assertFalse(is_valid)
 
     def test_nist_strong_password(self):
         # Testing a compliant password
-        is_valid, msg = check_password_nist("Dina_Secure_2026!")
+        is_valid, msg = check_password_nist("P@ssw0rd!2026")
         self.assertTrue(is_valid)
 
     def test_entropy_logic(self):
